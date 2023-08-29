978b84f57933b398d7b0ec759d9aabfe8c374beb
Maxim Alpatski <MaximAlpatski@gmail.com>
2023-08-29 18:34:36 +0400

    Add top bar

--- app/src/main/java/com/example/rickmasterstest/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/rickmasterstest/ui/screens/MainScreen.kt
@@ -6,36 +6,56 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rickmasterstest.R
 import com.example.rickmasterstest.ui.theme.RickMastersTestTheme
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier = Modifier, name: String) {
     val pagerState = rememberPagerState(pageCount = { 2 })
 
-    Column(Modifier.fillMaxSize()) {
-        HorizontalPagerBar(modifier = Modifier.fillMaxWidth(), state = pagerState)
-        HorizontalPager(state = pagerState) { page ->
-            // Our page content
-            Text(
-                text = "Page: $page",
-                modifier = Modifier.fillMaxWidth()
-            )
+    Scaffold(
+        topBar = { TopBar(stringResource(id = R.string.my_house)) }
+    ) { paddingValues ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(paddingValues)) {
+            HorizontalPagerBar(modifier = Modifier.fillMaxWidth(), state = pagerState)
+            HorizontalPager(state = pagerState) { page ->
+                // Our page content
+                Text(
+                    text = "Page: $page",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(title: String) {
+    TopAppBar(title = {
+        Text(modifier = Modifier.fillMaxWidth(), text = title, textAlign = TextAlign.Center)
+    })
 }
 
 @OptIn(ExperimentalFoundationApi::class)
