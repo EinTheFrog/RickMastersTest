b2524da3780c9c0cc9ebe264d372bae233ee6417
Maxim Alpatski <MaximAlpatski@gmail.com>
2023-08-30 20:04:57 +0400

    Add favorites icon

--- app/src/main/java/com/example/rickmasterstest/ui/screens/cameras/CamerasScreen.kt
+++ app/src/main/java/com/example/rickmasterstest/ui/screens/cameras/CamerasScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -98,13 +100,22 @@ fun CameraItem(camera: CameraDomain) {
         shape = RoundedCornerShape(16.dp)
     ) {
         Column {
-            Box {
+            Box(
+                contentAlignment = Alignment.TopEnd
+            ) {
                 AsyncImage(
                     modifier = Modifier.fillMaxWidth(),
                     contentScale = ContentScale.Crop,
                     model = camera.snapshot,
                     contentDescription = stringResource(id = R.string.camera_screenshot_description)
                 )
+                if (camera.favorites) {
+                    Image(
+                        modifier = Modifier.padding(16.dp).size(32.dp),
+                        painter = painterResource(id = R.drawable.star),
+                        contentDescription = stringResource(id = R.string.camera_favorite_description)
+                    )
+                }
             }
             Text(
                 modifier = Modifier.padding(16.dp),
